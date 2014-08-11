e616b4d7d9d72a52fee239785caaf605c8922ea7
georoot <rb1223334444@gmail.com>
2014-08-12 03:32:49 +0530

    basic example on modular programing

---
+++ components/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>
+				Welcome to finna-ninja
+			</title>
+		</head>
+		<body>
---
+++ components/home.php
@@ -0,0 +1,7 @@
+<style>
+	html body{
+		background-color:#8BF6FF;
+		color:#525252;
+	}
+</style>
+<div style="text-align:center"><h1>Seems like you are good to go</h1><div>
