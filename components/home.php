963ff5ab46ad83c3261e7dfb90aeb45844610dc7
Prakhar Bhardwaj <prakhar625@gmail.com>
2014-08-21 04:19:34 +0530

    added logo.png and logo.psd in static folder

--- components/home.php
+++ components/home.php
@@ -42,7 +42,7 @@
 
 <div class="container" style="text-align:center;">
 	<div class="jumbotron">
-        <h1>finna-ninja</h1>
+        <img src="logo.png" style="width: 250px;margin-bottom: 40px;">
         <p class="lead">Fina Ninja is a PHP Frame Work with super Cow Powers !! , It will help you to create your next big thing faster better and in a secure manner<br><br>
 Fina Ninja is still in the kung-fu school. so sit back and enjoy while we make him the best PHP framework . Know some tricks ? finna Ninja is ready to learn it from you. Go ahead , we are waiting for your pull request</p>
         <p><a class="btn btn-lg btn-primary" href="documentation" role="button">Start with Documentation...</a></p>
