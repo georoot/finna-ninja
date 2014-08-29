5aab4c889fefb3edf59da108538e82435d88395a
georoot <rb1223334444@gmail.com>
2014-08-29 18:36:29 +0530

    auth system added

---
+++ storage/lib/auth/auth.php
@@ -0,0 +1,50 @@
+<?php
+class auth{
+	public function __construct(){
+		session_name($GLOBALS['session_name']);
+		session_start();
+	}
+
+	public function signup($data){
+		if (isset($data["username"]) and isset($data["password"])) {
+			$data["password"] = password_hash($data["password"], PASSWORD_DEFAULT);
+			db::dbs("user") -> collection("data") -> insert($data);
+			return true;
+		}
+		return false;
+	}
+
+	public function login($username,$password){
+		$data = db::dbs("user") -> collection("data") -> find(array("username" => $username));
+		if (sizeof($data) == 1 and (password_verify($password, $data[0]["password"]))) {
+			$key = array_keys($data);
+			for ($i=0; $i < sizeof($key); $i++) { 
+				//all data buffered to session
+				$_SESSION[$key[$i]] = $data[$key[$i]];
+			}
+			$_SESSION["logged"] = true;
+			return true;
+		}
+		return false;
+	}
+
+	public function logout(){
+		$_SESSION["logged"] = false;
+		session_destroy();
+	}
+
+	public function get($key){
+		if (isset($_SESSION[$key])) {
+			return $_SESSION[$key];
+		}
+		return null;
+	}
+
+	public function is_logged(){
+		if (isset($_SESSION['logged']) and $_SESSION["logged"] == true) {
+			return true;
+		}
+		return false;
+	}
+}
+?>
