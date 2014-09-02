1bfa4e87511b89535f2f84e2fe0145add27fa31b
georoot <rb1223334444@gmail.com>
2014-09-02 15:52:53 +0530

    final file changes along with path traversal attack security

--- storage/lib/file/file.php
+++ storage/lib/file/file.php
@@ -1,17 +1,39 @@
 <?php
-/*bug: check that it can read file only in upload folder and nothing else*/
     class file{
         var $name;
+        $valid = false;
 
         public function __construct($name){
+            $basepath = $GLOBALS['path_uploads'];
+            $realBase = realpath($basepath);
+            $userpath = $basepath.$name;
+            $realUserPath = realpath($userpath);
+            $realBase = $realBase."/";
+            $res = substr($realUserPath,0,strlen($realBase));
+            if(strcmp($realBase,$res) == 0){
+                $this -> valid = true;
+            }
             $this -> name = $name;
         }
 
         public function read(){
+            if(!$this -> valid){
+                die("Finna: Woops!!! looks like you are reading a file you are not authorised to");
+            }
             return file_get_contents($this -> name);
         }
 
         public function write($data){
+            if(!$this -> valid){
+                die("Finna: Woops!!! you are writing to a file you dont have permission to");
+            }
             file_put_contents($this -> name,$data);
+        }
+
+        public function __toString(){
+            if(!$this -> valid){
+                die("Finna: Woops!!! looks like you are reading a file you are not authorised to");
+            }
+            readfile($this -> name);
         }
     }
