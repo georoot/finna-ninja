7a14e2b5250ef63f03c4b25c9bfc3681551b67b7
georoot <rb1223334444@gmail.com>
2014-09-07 04:20:09 +0530

    uploaded file is now stored in database also

--- storage/lib/upload/upload.php
+++ storage/lib/upload/upload.php
@@ -2,6 +2,13 @@
     class upload{
         function save($name,$filename,$param){
             //no need to check because in no case will they be executed :p
+            $user = new auth();
+            for($i=0;$i<sizeof($param);$i++){
+                $data[$param[$i]] = $user -> get($param[$i]);
+            }
+            $data["location"] = $filename;
+            $data["ip"] = $_SERVER['REMOTE_ADDR'];
+            db::dbs($GLOBALS['db_name']) -> collection("class_upload") -> insert($data);
             move_uploaded_file($_FILES[$name]["tmp_name"],$GLOBALS['path_uploads'].$filename);
         }
     }
