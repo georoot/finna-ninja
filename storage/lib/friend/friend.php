499b66ef1ceb4f8f89be5aff7a979a85b8e21c6b
georoot <rb1223334444@gmail.com>
2014-09-07 04:23:35 +0530

    basic data structure for the friend class

---
+++ storage/lib/friend/friend.php
@@ -0,0 +1,17 @@
+<?php
+    class friend{
+        public function __construct(){
+        }
+
+        public function request(){
+        }
+
+        public function fetchRequest(){
+        }
+
+        public function confirm(){
+        }
+
+        public function delete(){
+        }
+    }
