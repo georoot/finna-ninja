bd8272fdca99c9d5dec07d7bdebfe7c055cf7ced
georoot <rb1223334444@gmail.com>
2014-09-07 04:28:31 +0530

    changes in friend lib

--- storage/lib/friend/friend.php
+++ storage/lib/friend/friend.php
@@ -1,17 +1,39 @@
 <?php
+    /*Dependency: auth,db*/
     class friend{
+        var $auth;
         public function __construct(){
+            $this -> auth = new auth();
         }
 
-        public function request(){
+        public function request($username){
+            if($this -> auth ->is_logged()){
+                //send request and return true
+            }
+            //not logged return false
         }
 
         public function fetchRequest(){
+            if($this -> auth ->is_logged()){
+                //fetch friend req associated with this account
+            }
         }
 
-        public function confirm(){
+        public function confirm($username){
+            if($this -> auth ->is_logged()){
+                //confirm the friend request of whosoever is sending it
+            }
         }
 
-        public function delete(){
+        public function delete($username){
+            if($this -> auth ->is_logged()){
+                //Delete the friend request
+            }
+        }
+
+        public function fetchFriends(){
+            if($this -> auth ->is_logged()){
+                //fetch all friends associated with this account
+            }
         }
     }
