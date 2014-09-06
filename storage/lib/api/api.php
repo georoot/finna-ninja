b1a3f6e01026254bb22aed96d522bcd19ff24cf6
georoot <rb1223334444@gmail.com>
2014-09-07 00:01:02 +0530

    basic generation in api

--- storage/lib/api/api.php
+++ storage/lib/api/api.php
@@ -8,19 +8,24 @@
         public function registerApp($information){
             //generate a public and private key
             $public = uniqid($GLOBALS['app_name']);
-            $private = openssl_random_pseudo_bytes(400);
-            print $public."<br>Private<br>".$private;
+            $private = openssl_random_pseudo_bytes(20);
+            return array("public" => $public,"private" => $private);
         }
 
         public function generateAuthorizationCode($publicKey){
             //generate a token for the user
+            $token = uniqid(uniqid());
+            return $token;
         }
 
         public function generateSecurityToken($authorizationToken,$privateKey){
             //generate an access Token to access user information
+            $secToken = openssl_random_pseudo_bytes(200);
+            return $secToken;
         }
 
         public function serveRequest($securityToken,$privateToken){
             //serve the user information as required
+            //not sure on what should be going here making this program linked to an interface for the cloud
         }
     }
