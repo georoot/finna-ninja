3faebb367c1ef4dd2576623f5bd5c04498bed333
georoot <rb1223334444@gmail.com>
2014-09-06 18:28:08 +0530

    template for follow class

---
+++ storage/lib/follow/follow.php
@@ -0,0 +1,15 @@
+<?php
+    class follow{
+        public function __construct(){
+        }
+
+        public function startFollowing($username){
+        }
+
+        public function getFollowers($username){
+        }
+
+        public function stopFollowing($username){
+        }
+    }
+
