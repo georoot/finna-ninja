f005c4914a9e0f6315fbd4b8eec8fdde26b5a28a
georoot <rb1223334444@gmail.com>
2014-12-07 20:17:56 +0530

    tag cloud

---
+++ storage/lib/tagCloud/tagCloud.php
@@ -0,0 +1,16 @@
+<?php
+/*Dependency: db*/
+/*TODO:
+complete this class
+*/
+class tagCloud{
+    var $output = 5;
+    public function __construct(){
+    }
+
+    public function search($text){
+    }
+
+    public function add($tag){
+    }
+}
